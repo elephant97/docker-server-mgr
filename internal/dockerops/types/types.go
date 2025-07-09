c1312c65e8bd9799cbf0e457c0008769c1e3fb9d
elephant97 <elephant0908@gmail.com>
2025-07-09 22:36:33 +0900

    FEAT: delete api 구현
    * containter delete 요청 처리 api 구현

--- internal/dockerops/types/types.go
+++ internal/dockerops/types/types.go
@@ -6,3 +6,10 @@ type ContainerInfo struct {
 	ID     string         `db:"id"`
 	Status sql.NullString `db:"status"`
 }
+
+type ContainerStatus struct {
+	ID        string         `db:"id"`
+	Status    sql.NullString `db:"status"`
+	Name      sql.NullString `db:"container_name"`
+	DeletedAt sql.NullTime   `db:"deleted_at"`
+}
