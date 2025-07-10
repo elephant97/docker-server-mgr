efae4d1c2d5d2155f230158298e44e34a02994df
elephant97 <elephant0908@gmail.com>
2025-07-10 17:05:23 +0900

    FEAT: create request에 데이터 추가 및 json 파싱 이름 변경
    * cmd -> cmds로 변경
    * user가 생성한 컨테이너 조회할 수 있도록 user_id 받아 저저장하도록 추가

--- internal/common/request/types.go
+++ internal/common/request/types.go
@@ -6,10 +6,11 @@ type PortMapping struct {
 }
 
 type CreateRequest struct {
+	UserId        int64         `json:"user_id"` // 사용자 ID
 	Image         string        `json:"image"`
 	Tag           string        `json:"tag"`            // Docker 이미지 태그
 	ContainerName string        `json:"container_name"` // 컨테이너 이름
-	Cmd           []string      `json:"cmd"`
+	Cmd           []string      `json:"cmds"`
 	Ports         []PortMapping `json:"ports"`   // 포트 매핑 정보
 	TTL           int           `json:"ttl"`     // 단위: 초
 	Volumes       []string      `json:"volumes"` // 호스트 디렉토리 바인딩
